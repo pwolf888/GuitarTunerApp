825fcfe46cdb9b7bd510aaadbbcecbb14c801df3
JonathanTurnbull <jonathan.turnbul8@gmail.com>
2017-08-23 16:12:56 +1000

    First Commit
    Guitar Tuner App

---
+++ MyApplication/app/src/main/java/com/example/jonathanturnbull/guitarturnerapp/FrontPage.java
@@ -0,0 +1,13 @@
+package com.example.jonathanturnbull.guitarturnerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrontPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front_page);
+    }
+}
