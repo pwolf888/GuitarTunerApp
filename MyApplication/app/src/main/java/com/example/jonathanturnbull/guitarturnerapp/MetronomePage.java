b19841c1b9f85459ddd7f267a0ee68c43ffcf90b
JonathanTurnbull <jonathan.turnbul8@gmail.com>
2017-08-24 13:22:42 +1000

    Add in the activities needed

---
+++ MyApplication/app/src/main/java/com/example/jonathanturnbull/guitarturnerapp/MetronomePage.java
@@ -0,0 +1,13 @@
+package com.example.jonathanturnbull.guitarturnerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MetronomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metronome_page);
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/jonathanturnbull/guitarturnerapp/RecordPage.java
@@ -0,0 +1,13 @@
+package com.example.jonathanturnbull.guitarturnerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_page);
+    }
+}
