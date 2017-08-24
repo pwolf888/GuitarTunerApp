410efee35c1bc4fd5a4d6ebe12bd30d214ed92e9
JonathanTurnbull <jonathan.turnbul8@gmail.com>
2017-08-24 16:35:11 +1000

    Setting up an inflater menu

--- MyApplication/app/src/main/java/com/example/jonathanturnbull/guitarturnerapp/FrontPage.java
+++ MyApplication/app/src/main/java/com/example/jonathanturnbull/guitarturnerapp/FrontPage.java
@@ -1,13 +1,23 @@
 package com.example.jonathanturnbull.guitarturnerapp;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class FrontPage extends AppCompatActivity {
 
+public class FrontPage extends Fragment {
+
+    View myView;
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.frontpage);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.frontpage, container, false);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
--- MyApplication/app/src/main/java/com/example/jonathanturnbull/guitarturnerapp/LoginPage.java
+++ MyApplication/app/src/main/java/com/example/jonathanturnbull/guitarturnerapp/LoginPage.java
@@ -1,7 +1,9 @@
 package com.example.jonathanturnbull.guitarturnerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+    }
+
+    public void goToFrontpage(View view) {
+        Intent intent = new Intent(this, FrontPage.class);
+        startActivity(intent);
     }
 }
