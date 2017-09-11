31c8ad424d5e06e1f2ecace475a8f52ae43a5249
JonathanTurnbull <jonathan.turnbul8@gmail.com>
2017-09-11 13:18:39 +1000

    added in the user data table

--- MyApplication/app/src/main/java/com/example/jonathanturnbull/guitarturnerapp/DBHelper.java
+++ MyApplication/app/src/main/java/com/example/jonathanturnbull/guitarturnerapp/DBHelper.java
@@ -10,12 +10,29 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
 
+    // My UserData Table and columns
+    public static final String TABLE_USERDATA = "userdata";
+    public static final String COLUMN_USER_ID = "_userid";
+    public static final String COLUMN_USERNAME = "username";
+    public static final String COLUMN_PASSW = "password";
+
+
+    // My lyrics table and columns
     public static final String TABLE_LYRICS = "lyrics";
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_TITLE = "songtitle";
     public static final String COLUMN_SONG = "lyricstext";
-    private static final String DATABASE_NAME = "lyric.db";
+
+
+    private static final String DATABASE_NAME = "userdata.db";
     private static final int DATABASE_VERSION = 1;
+
+
+    private static final String CREATE_USERDATA_TABLE = "create table " + TABLE_USERDATA
+            + "(" + COLUMN_USER_ID + " integer primary key autoincrement, "
+            + COLUMN_USERNAME + " text not null, "
+            + COLUMN_PASSW + " text not null);";
+
     // Database creation sql statement
     private static final String DATABASE_CREATE = "create table " + TABLE_LYRICS
             + "(" + COLUMN_ID + " integer primary key autoincrement, "
@@ -27,10 +44,11 @@ public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(Context context) { super(context, DATABASE_NAME, null, DATABASE_VERSION); }
 
     @Override
-    public void onCreate(SQLiteDatabase db) { db.execSQL(DATABASE_CREATE); }
+    public void onCreate(SQLiteDatabase db) {  db.execSQL(CREATE_USERDATA_TABLE); db.execSQL(DATABASE_CREATE); }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERDATA);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_LYRICS);
         onCreate(db);
     }
