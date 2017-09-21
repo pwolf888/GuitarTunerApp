5b2426431b00f2b8027334788f77f17d161317e6
JonathanTurnbull <jonathan.turnbul8@gmail.com>
2017-09-21 16:59:30 +1000

    added in the metronome page

--- MyApplication/app/src/main/java/com/example/jonathanturnbull/guitarturnerapp/MetronomePage.java
+++ MyApplication/app/src/main/java/com/example/jonathanturnbull/guitarturnerapp/MetronomePage.java
@@ -1,11 +1,15 @@
 package com.example.jonathanturnbull.guitarturnerapp;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class MetronomePage extends AppCompatActivity {
 
@@ -13,7 +17,22 @@ public class MetronomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metronome_page);
+
+        MediaPlayer metronomeSound = MediaPlayer.create(this, R.raw.metronomesound);
+
+
+
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.switch_Metro);
+        toggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                metronomeSound.start();
+            } else {
+                metronomeSound.pause();
+            }
+        });
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
